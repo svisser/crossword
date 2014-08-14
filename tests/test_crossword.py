86273d96e33e3bd686904377ba2b53fbbbcbc38b
Simeon Visser <simeon87@gmail.com>
2014-08-14 23:36:47 +0100

    Use a better variable name instead of one character

--- tests/test_crossword.py
+++ tests/test_crossword.py
@@ -6,6 +6,6 @@ from crossword import Crossword
 class CrosswordTestCase(unittest.TestCase):
 
     def test_crossword_set_and_get_element(self):
-        c = Crossword(10, 10)
-        c[3, 3] = 'A'
-        self.assertEqual(c[3, 3], 'A')
+        crossword = Crossword(10, 10)
+        crossword[3, 3] = 'A'
+        self.assertEqual(crossword[3, 3], 'A')
