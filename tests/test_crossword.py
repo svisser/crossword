b1e5b8ab72a99ab01384bbae017ecbdca52b5826
Simeon Visser <simeon87@gmail.com>
2014-08-14 23:35:51 +0100

    Renamed test

--- tests/test_crossword.py
+++ tests/test_crossword.py
@@ -5,7 +5,7 @@ from crossword import Crossword
 
 class CrosswordTestCase(unittest.TestCase):
 
-    def test_basic_crossword_set_and_get_element(self):
+    def test_crossword_set_and_get_element(self):
         c = Crossword(10, 10)
         c[3, 3] = 'A'
         self.assertEqual(c[3, 3], 'A')
