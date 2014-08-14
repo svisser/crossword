580a222c869e3c418b20a1d557f264421c4720dc
Simeon Visser <simeon87@gmail.com>
2014-08-14 22:58:40 +0100

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='crossword',
+    version='0.1',
+    description='Python library for handling crossword puzzles',
+    url='https://github.com/svisser/crossword',
+    author='Simeon Visser',
+    author_email='simeonvisser@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='crossword puzzle'
+)
