e801adb3e4a52b109967baa44419753187f65ff0
Simeon Visser <simeon87@gmail.com>
2014-08-14 23:34:27 +0100

    Added Dublin Core metadata fields as a standard way of representing metadata properties

--- crossword/__init__.py
+++ crossword/__init__.py
@@ -18,6 +18,23 @@ class Crossword(object):
         self.height = height
         self._data = [[None for x in range(width)] for x in range(height)]
 
+        # Dublin Core Metadata Element Set, Version 1.1
+        self.contributor = None
+        self.coverage = None
+        self.creator = None
+        self.date = None
+        self.description = None
+        self.format = None
+        self.identifier = None
+        self.language = None
+        self.publisher = None
+        self.relation = None
+        self.rights = None
+        self.source = None
+        self.subject = None
+        self.title = None
+        self.type = None
+
     def __getitem__(self, index):
         x, y = index
         return self._data[y][x]
