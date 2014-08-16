e722736033c801c63a705eee23914edbefa33ecb
Simeon Visser <simeon87@gmail.com>
2014-08-16 23:02:37 +0100

    Added test to clarify default ordering of clues

--- tests/test_crossword.py
+++ tests/test_crossword.py
@@ -150,3 +150,17 @@ class CrosswordTestCase(unittest.TestCase):
                 'down': {},
             }
         })
+
+    def test_clues_are_sorted_in_numerical_order(self):
+        crossword = Crossword(15, 15)
+        crossword.clues.across[1] = "Clue 1"
+        crossword.clues.across[10] = "Clue 10"
+        crossword.clues.across[2] = "Clue 2"
+        self.assertEqual(
+            list(crossword.clues.across()),
+            [
+                (1, "Clue 1"),
+                (2, "Clue 2"),
+                (10, "Clue 10"),
+            ]
+        )
