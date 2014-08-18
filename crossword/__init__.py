76c46cf95558167104b816ef22fa57b322c732ea
Simeon Visser <simeon87@gmail.com>
2014-08-18 20:17:39 +0100

    Reuse iteration logic as specified by cells property

--- crossword/__init__.py
+++ crossword/__init__.py
@@ -196,9 +196,8 @@ def from_ipuz(ipuz_dict):
 
     for key in ('puzzle', 'solution'):
         entry = ipuz_dict.get(key)
-        for y in range(crossword.height):
-            for x in range(crossword.width):
-                crossword[x, y][key] = entry[y][x] if entry is not None else None
+        for x, y in crossword.cells:
+            crossword[x, y][key] = entry[y][x] if entry is not None else None
 
     for key, value in ipuz_dict.items():
         if key not in known_keys:
