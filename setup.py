66f4663487267120b620db42e3666d76ef5c3a31
Simeon Visser <simeon87@gmail.com>
2014-08-23 17:30:33 +0100

    Added long_description field to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
+from os import path
 from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as file_readme:
+    long_description = file_readme.read()
 
 
 setup(
     name='crossword',
     version='0.1.2',
     description='Python library for handling crossword puzzles',
+    long_description=long_description,
     url='https://github.com/svisser/crossword',
     author='Simeon Visser',
     author_email='simeonvisser@gmail.com',
