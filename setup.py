acc57a462da46e10e5d8787273f158f0a5038863
Simeon Visser <simeon87@gmail.com>
2014-08-24 05:20:06 +0100

    Added keyword to setup.py

--- setup.py
+++ setup.py
@@ -28,5 +28,5 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
     ],
-    keywords='crossword puzzle ipuz'
+    keywords='crossword puzzle ipuz puz'
 )
